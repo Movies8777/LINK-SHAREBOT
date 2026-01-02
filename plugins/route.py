3d69ae1ec581b24be82a3657e9668df3444ffbd3
Movies8777 <roxedits07@gmail.com>
2026-01-02 21:44:14 +0530

    Update root route response message

--- plugins/route.py
+++ plugins/route.py
@@ -4,4 +4,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("RexBots - Abhi,Master,Seishiro")
+    return web.json_response("𓃵 - ♚𝖧υηтєʀ⚰️")
